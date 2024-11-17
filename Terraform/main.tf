123975387aee9daa0c116ff3199e3ed9cfa566d3
maryuriser <maryuriserpa15@gmail.com>
2024-11-17 18:16:59 -0500

    deploy kai

---
+++ Terraform/main.tf
@@ -0,0 +1,90 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3"{
+    key= "aws/ec2-deploy/terraform.tfstate"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "kaiserver" {
+  ami = "ami-0ea3c35c5c3284d82"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [ aws_security_group.kaimaingroup.id ]
+  iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
+  connection {
+    type = "ssh"
+    host = self.public_ip
+    user = "ubuntu"
+    private_key = var.private_key
+    timeout = "4m"
+  }
+  tags = {
+    "name" = "DeployVM"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2-profile" {
+  name = "ec2-profile"
+  role = "EC2-ECR-KAI"
+}
+
+resource "aws_security_group" "kaimaingroup" {
+  egress = {
+    {
+      cidr_blocks = ["0.0.0.0/0"]
+      description = ""
+      from_port = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      protocol = "-1"
+      security_groups = []
+      self = false
+      to_port = 0
+    }
+  }
+  ingress = {
+    {
+      cidr_blocks = ["0.0.0.0/0", ]
+      description = ""
+      from_port =22
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      protocol = "tcp"
+      security_groups = []
+      self = false
+      to_port = 22
+    },
+    {
+      cidr_blocks = ["0.0.0.0/0", ]
+      description = ""
+      from_port =802
+      ipv6_cidr_blocks = []
+      prefix_list_ids = []
+      protocol = "tcp"
+      security_groups = []
+      self = false
+      to_port = 80
+    }
+  
+    }
+} 
+
+resource "aws_key_pair" "deployer" {
+  key_name = var.key_name
+  public_key = var.public_key
+}
+
+output "instance_public_ip" {
+  value = aws_instance.kaiserver.public_ip
+  sensitive = true
+}
---
+++ Terraform/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+    default = "us-east-2"
+}
+
+variable "public_key" {
+  
+}
+variable "private_key" {
+  
+}
+variable "key_name" {
+  
+}
